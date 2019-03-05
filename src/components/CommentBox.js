0d5607bfb23e7449bc2b9625bd31b0cac68c076b
aaron <xakong@outlook.com>
2019-03-05 08:22:29 -0500

    more initial setup

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-   return <div>Comment Box</div>
-};
+class CommentBox extends Component {
+   state = {
+      comment: ''
+   };
+
+   handleChange = (e) => {
+      this.setState({ comment: e.target.value})
+   };
+
+   handleSubmit = (e) => {
+      e.preventDefault(); // prevents page from attempting to reload
+
+      // TODO- Call an action creator - redux
+      // and save the comment
+
+      this.setState({ comment: '' });
+   };
+
+   render() {
+      return (
+         <form onSubmit={this.handleSubmit}>
+            <h4>Add a Comment</h4>
+            <textarea 
+               value={this.state.comment} 
+               onChange={this.handleChange} 
+            />
+            <div>
+               <button>Submit Comment</button>
+            </div>
+         </form>
+      )
+   }
+}
+
+export default CommentBox;
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+// must init let wrapped outside of beforeEach scope
+let wrapped;
+
+// beforeEach runs before each it() test. 
+beforeEach(() => {
+   wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-   const wrapped = shallow(<App />);
-
    expect(wrapped.find(CommentBox).length).toEqual(1);
    // returns a length of 1 commentBox, that equals 1
 });
 
 it('shows a comment list', () => {
-   const wrapped = shallow(<App />);
-
    expect(wrapped.find(CommentList).length).toEqual(1);
 })
