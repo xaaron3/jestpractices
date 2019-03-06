4883296d218a4119f254753b0f616d101c987507
aaron <xakong@outlook.com>
2019-03-06 08:44:16 -0500

    moxios

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
-   // attempt to render the entire app
+beforeEach(() => {
+   moxios.install();
+   moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+      status: 200,
+      response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
+   });
+});
+
+afterEach(() => {
+   moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', (done) => {
    const wrapped = mount(
       <Root>
          <App />
       </Root>
    );
 
-   // find the 'fetchComments' button and click it
    wrapped.find('.fetch-comments').simulate('click');
 
-   // expect to find a list of comments, 500 LIs
-   expect(wrapped.find('li').length).toEqual(500);
+   // introduce a TINY pause for moxios to install
+   moxios.wait(() => {
+      wrapped.update();
+
+      expect(wrapped.find('li').length).toEqual(2);
+
+      done();
+      wrapped.unmount();
+   });
 });
